24e6c20826680c3d00f35ac33c8f142e7f3cf2a9
Сергій <kjserg@i.ua>
2024-10-22 18:30:11 +0300

    main.html

--- js/app.js
+++ js/app.js
@@ -1,4 +1,87 @@
 let previousZoom = window.devicePixelRatio;
+const doc = document;
+addEventListener("DOMContentLoaded", (event) => {
+    hideLoader();
+    toggleInputPassword();
+    detectFirstLoadZoom();
+    doc.querySelectorAll('.sub-menu').forEach(function (element, index) {
+        const a = element.firstElementChild;
+        const list = element.querySelector('.sub-list');
+        a.addEventListener('click', function (e) {
+            e.preventDefault();
+            if (element.classList.contains('open')) {
+                element.classList.remove("open");
+                a.classList.remove("active");
+                slideUp(list, 500);
+            } else {
+                hideElementsSidebar({
+                    exclude: index
+                });
+                element.classList.add("open");
+                a.classList.add("active");
+                slideDown(list, 500);
+            }
+        });
+    });
+    doc.querySelectorAll('.hide-element').forEach(function (element, index) {
+        element.addEventListener('click', function (e) {
+            e.preventDefault();
+            const href = element.getAttribute('href');
+            const el = doc.querySelector(href);
+            if (el) {
+                if (el.classList.contains('sidebar') && window.innerWidth <= 560) {
+                    el.classList.remove('show');
+                } else {
+                    el.classList.add('hidden-element');
+                    slideLeft(el, 400);
+                }
+            }
+        });
+    });
+    doc.querySelectorAll('.show-element').forEach(function (element, index) {
+        element.addEventListener('click', function (e) {
+            e.preventDefault();
+            const href = element.getAttribute('href');
+            const el = doc.querySelector(href);
+            if (el) {
+                el.classList.remove('hidden-element');
+                slideRight(el, 400);
+            }
+        });
+    });
+    doc.querySelectorAll('.burger').forEach(function (element, index) {
+        element.addEventListener('click', function (e) {
+            e.preventDefault();
+            const href = element.getAttribute('href');
+            const el = doc.querySelector(href);
+            if (el) {
+                el.removeAttribute('style');
+                el.classList.add('show');
+            }
+        });
+    });
+});
+
+window.addEventListener('resize', () => {
+    detectPreviousZoom();
+});
+
+function hideElementsSidebar(args) {
+    let exclude = -1;
+    if (args.hasOwnProperty('exclude')) {
+        exclude = args.exclude;
+    }
+    doc.querySelectorAll('.sub-menu').forEach(function (element, index) {
+        if (exclude !== index) {
+            const a = element.firstElementChild;
+            const list = element.querySelector('.sub-list');
+            element.classList.remove("open");
+            a.classList.remove("active");
+            slideUp(list, 200);
+        }
+
+    });
+}
 
 function toggleInputPassword() {
     document.querySelectorAll('.form-password__button').forEach(function (element, key, parent) {
@@ -18,6 +101,7 @@ function toggleInputPassword() {
 }
 
 function hideLoader() {
+    if (document.getElementsByClassName('authorization-image-load__progress').length === 0) return;
     document.getElementsByClassName('authorization-image-load__progress')[0].classList.add('loaded');
     setTimeout(function () {
         document.getElementsByClassName('authorization-image')[0].classList.add('hidden-sm');
@@ -32,38 +116,115 @@ function hideLoader() {
 function detectPreviousZoom() {
     let currentZoom = window.devicePixelRatio;
     if (currentZoom !== previousZoom) {
-        console.log('Zoom level changed!');
         previousZoom = Number(currentZoom.toFixed(2));
-        console.log(previousZoom);
         var fontSize = 10;
         fontSize = fontSize * previousZoom;
         fontSize = Number(fontSize.toFixed(0));
-        console.log(fontSize);
         document.querySelectorAll('html')[0].setAttribute('data-zoom-level', previousZoom);
     }
 }
+
 function detectFirstLoadZoom() {
     let currentZoom = window.devicePixelRatio;
-    console.log(currentZoom);
     if (currentZoom !== 1) {
-        console.log('Zoom level changed!');
         previousZoom = Number(currentZoom.toFixed(2));
-        console.log(previousZoom);
         var fontSize = 10;
         fontSize = fontSize * previousZoom;
         fontSize = Number(fontSize.toFixed(0));
-        console.log(fontSize);
         document.querySelectorAll('html')[0].setAttribute('data-zoom-level', previousZoom);
     }
 }
 
-addEventListener("DOMContentLoaded", (event) => {
-    hideLoader();
-    toggleInputPassword();
-    detectFirstLoadZoom();
-});
+function slideDown(element, duration = 400) {
+    element.style.removeProperty('display');
+    let display = window.getComputedStyle(element).display;
 
-window.addEventListener('resize', () => {
-    detectPreviousZoom();
-});
+    if (display === 'none') {
+        element.style.display = 'block';
+    }
+
+    let height = element.offsetHeight;
+    element.style.overflow = 'hidden';
+    element.style.height = 0;
+    element.style.transition = `height ${duration}ms ease`;
+
+    setTimeout(() => {
+        element.style.height = height + 'px';
+    }, 10);
+
+    setTimeout(() => {
+        element.style.removeProperty('height');
+        element.style.removeProperty('overflow');
+        element.style.removeProperty('transition');
+    }, duration);
+}
+
+function slideRight(element, duration = 400) {
+    element.style.removeProperty('display');
+    let display = window.getComputedStyle(element).display;
+
+    if (display === 'none') {
+        element.style.display = 'block';
+    }
+
+    let width = element.offsetWidth;
+    element.style.overflow = 'hidden';
+    element.style.width = 0;
+    element.style.transition = `width ${duration}ms ease`;
+
+    setTimeout(() => {
+        element.style.width = width + 'px';
+    }, 10);
+
+    setTimeout(() => {
+        element.style.removeProperty('width');
+        element.style.removeProperty('overflow');
+        element.style.removeProperty('transition');
+    }, duration);
+}
+
+function slideUp(element, duration = 400) {
+    element.style.height = element.offsetHeight + 'px';
+    element.style.overflow = 'hidden';
+    element.style.transition = `height ${duration}ms ease`;
+
+    setTimeout(() => {
+        element.style.height = 0;
+    }, 10);
+
+    setTimeout(() => {
+        element.style.display = 'none';
+        element.style.removeProperty('height');
+        element.style.removeProperty('overflow');
+        element.style.removeProperty('transition');
+    }, duration);
+}
+
+function slideLeft(element, duration = 400) {
+    element.style.width = element.offsetWidth + 'px';
+    element.style.overflow = 'hidden';
+    element.style.transition = `width ${duration}ms ease`;
+
+    setTimeout(() => {
+        element.style.width = 0;
+    }, 10);
+
+    setTimeout(() => {
+        element.style.display = 'none';
+        element.style.removeProperty('width');
+        element.style.removeProperty('overflow');
+        element.style.removeProperty('transition');
+    }, duration);
+}
+
+function slideToggle(element, duration = 400) {
+    if (window.getComputedStyle(element).display === 'none') {
+        slideDown(element, duration);
+    } else {
+        slideUp(element, duration);
+    }
+}
+
+
+
 
